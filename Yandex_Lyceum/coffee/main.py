e53b5bd6bff2b6f42ccac32c16205cc8d4850687
Tleukhan <maysadlysw@gmail.com>
2022-12-21 02:05:13 +0600

    modi

--- Yandex_Lyceum/coffee/main.py
+++ Yandex_Lyceum/coffee/main.py
@@ -1,7 +1,31 @@
 import sqlite3
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QWidget
+
+
+class Form(QWidget):
+    def __init__(self, dbv):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.dbv = dbv
+        self.pushButton.clicked.connect(self.add_coffee)
+
+    def add_coffee(self):
+        a = self.lineEdit.text()
+        b = self.lineEdit_2.text()
+        c = self.comboBox.currentText()
+        d = self.lineEdit_4.text()
+        e = self.lineEdit_5.text()
+        f = self.lineEdit_6.text()
+        if a != '' and b != '' and d != '' and e != '' and f != '':
+            query = '''INSERT INTO
+            coffee(varieties, roast_level, processing, taste_description, price, package_size)
+            VALUES(?, ?, ?, ?, ?, ?)'''
+            self.dbv.connection.cursor().execute(query, (a, b, c, d, e, f))
+            self.dbv.select_data()
+            self.dbv.connection.commit()
+            self.close()
 
 
 class DBSample(QMainWindow):
@@ -11,12 +35,14 @@ class DBSample(QMainWindow):
         self.connection = sqlite3.connect("coffee.db")
         self.pushButton.clicked.connect(self.select_data)
         self.textEdit.setPlainText("SELECT * FROM coffee")
+        self.pushButton_2.clicked.connect(self.add_coffee)
         self.select_data()
+        self.form = Form(self)
 
     def select_data(self):
         query = self.textEdit.toPlainText()
         res = self.connection.cursor().execute(query).fetchall()
-        self.tableWidget.setColumnCount(5)
+        self.tableWidget.setColumnCount(6)
         self.tableWidget.setRowCount(0)
         for i, row in enumerate(res):
             self.tableWidget.setRowCount(
@@ -24,6 +50,10 @@ class DBSample(QMainWindow):
             for j, elem in enumerate(row):
                 self.tableWidget.setItem(
                     i, j, QTableWidgetItem(str(elem)))
+    
+    def add_coffee(self):
+        self.form.show()
+        self.textEdit.setPlainText("SELECT * FROM coffee")
 
     def closeEvent(self, event):
         self.connection.close()
